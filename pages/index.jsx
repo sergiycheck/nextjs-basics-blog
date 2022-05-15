acce1646cc4867206516d557cfe159ba8bbc3ee7
serhii.kuzmych <sergiycheck@gmail.com>
2022-05-15 15:41:28 +0300

    polished styles for  pages

---
+++ pages/index.jsx
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <nav>
+        Read{' '}
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
+      </nav>
+
+      <section className={utilStyles.headingMd}>
+        <p>Some words about me</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
